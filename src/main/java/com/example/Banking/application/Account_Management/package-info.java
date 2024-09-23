4de1458b1bc0fd6daba2c2c7033746a2025381a4
mabdelSE450 <mabdels1@depaul.edu>
2024-09-23 10:58:09 -0500

    Added package-info.java file and a test class for account management

---
+++ src/main/java/com/example/Banking/application/Account_Management/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * This package contains classes related to account management in the banking application.
+ * <p>
+ * The key classes in this package include:
+ * </p>
+ * <ul>
+ *   <li><b>AccountCreation:</b> Allows users to create a checking or savings account.</li>
+ *   <li><b>UpdateAccount:</b> Enables users to update profile information such as address, name, 
+ *   security questions, and overall preferences.</li>
+ *   <li><b>ViewBalance:</b> Provides users with the ability to view their current account balance.</li>
+ * </ul>
+ * <p>
+ * Future enhancements include the use of Lombok to remove repetitive boilerplate code and 
+ * JPA for simpler database interactions.
+ * </p>
+ * 
+ * @author Mohammad Abdelsalam
+ */
+package com.example.Banking.application.Account_Management;
