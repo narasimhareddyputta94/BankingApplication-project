01ab5ac1022114835ca528fda5ee2d54f532bff5
Shashank Datt <122945500+ShashankDatt08@users.noreply.github.com>
2024-09-23 16:25:19 -0500

    Add files via upload
    UserAuthentication

---
+++ src/main/java/com/example/Banking/application/Authentication/UserSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.example.Banking.application.Autentication;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@Configuration
+public class UserSecurityConfig {
+
+}
