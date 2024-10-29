3dca739902f5c559bf814f2452e35fa3db634446
Shashank <shashankdatt08@gmail.com>
2024-10-28 23:09:14 -0500

    Added Test Class

--- src/main/java/com/example/Banking/application/authentication/UserService.java
+++ src/main/java/com/example/Banking/application/authentication/UserService.java
@@ -4,9 +4,9 @@ import com.example.Banking.application.accountManagement.AccountCreation;
 import com.example.Banking.application.accountManagement.AccountCreationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.Random;
 
 @Service
@@ -21,10 +21,14 @@ public class UserService {
 	@Autowired
 	UserSecurityConfig userSecurityConfig;
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
 	public User registerUser(String name , String password , String email, AccountCreation.AccountType accountType, long balance) {
+		String encodedPassword = bCryptPasswordEncoder.encode(password);
 		User user = new User();
 		user.setUsername(name);
-		user.setPassword(userSecurityConfig.passwordEncoder().encode(password));
+		user.setPassword(encodedPassword);
 		user.setEmail(email);
 		user.setAccountType(accountType);
 		user.setBalance(balance);
@@ -37,7 +41,7 @@ public class UserService {
 				.accountType(accountType)
 				.accountNumber(accountNumber)
 				.balance(balance)
-				.createOn(LocalDateTime.now())
+				.createOn(LocalDate.now())
 				.build();
 		AccountCreation saveAccount = accountRepo.save(accountCreation);
 		saveUser.setAccount(saveAccount);
