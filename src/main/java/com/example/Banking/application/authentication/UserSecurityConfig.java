22e749e05fa067e3675a83bb7a581479aa455d25
Shashank <shashankdatt08@gmail.com>
2024-11-18 14:15:11 -0600

    edited Api access

--- src/main/java/com/example/Banking/application/authentication/UserSecurityConfig.java
+++ src/main/java/com/example/Banking/application/authentication/UserSecurityConfig.java
@@ -24,7 +24,7 @@ public class UserSecurityConfig {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/index.html").permitAll()
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/singup" , "/auth/login" , "/auth/validate" , "/auth/logout").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
