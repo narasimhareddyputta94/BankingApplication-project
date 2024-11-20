63769831167b144793554816d83c16ff681c3dd7
mabdelSE450 <mabdels1@depaul.edu>
2024-11-19 19:46:00 -0600

    Update AccountCreation.java

--- src/main/java/com/example/banking/application/accountManagement/AccountCreation.java
+++ src/main/java/com/example/banking/application/accountManagement/AccountCreation.java
@@ -18,9 +18,9 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name = "Accounts", uniqueConstraints = {
-		@UniqueConstraint(columnNames = {"userId", "accountType"}),
+	    @UniqueConstraint(columnNames = {"userId", "accountType"}),
 		@UniqueConstraint(columnNames = {"accountNumber"})
-})
+	})
 
 //@Table(name = "Accounts")
 @NoArgsConstructor
@@ -32,30 +32,30 @@ public class AccountCreation {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long accountId;
 
-
-	@OneToOne(fetch = FetchType.EAGER)
+	
+	@OneToOne
 	@JoinColumn(name = "userId")
 	@NotNull
 	private User user;
 	@NotNull(message = "Account type is required")
 	//private String accountType;
-	//@Enumerated(EnumType.STRING)
+	@Enumerated(EnumType.STRING)
 	private AccountType accountType;
 	@NotNull(message = "Balance is required")
 	private Long balance;
 	@NotNull(message = "Creation date is required")
 	private LocalDate createOn;
-	// CHeck changing LocalDateTime to LocalDate, check testing cases for errors
+	
 
 	@NotNull
 	private String accountNumber;
 
-
+	
 	public enum AccountType {
-		CHECKINGS,
-		SAVINGS
-	}
-
+        CHECKINGS,
+        SAVINGS
+        }
+	
 
 	public AccountType getAccountType() {
 		return accountType;
